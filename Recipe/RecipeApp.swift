89be1f503211b8702d66a1610a3f3af09826365b
Jeehun Choi <hobbit0717@gmail.com>
2021-07-13 22:40:49 -0400

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by JH on 2021-07-13.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
